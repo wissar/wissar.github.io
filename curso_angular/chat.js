7d1a8c894b5f88a836132faec2fea32d8c98d690
wissar <masterk850@gmail.com>
2016-01-10 16:47:21 -0600

    Cambiado el tamaño del text

--- curso_angular/chat.js
+++ curso_angular/chat.js
@@ -2,4 +2,5 @@ angular.module('myApp', ['lumx', 'firebase'])
     .controller('ctrlChat', function($scope, $firebaseArray) {
     	var ref = new Firebase('https://wissar-chat.firebaseio.com');
     	$scope.messages = $firebaseArray(ref);
+    	console.log($scope.messages);
     })
