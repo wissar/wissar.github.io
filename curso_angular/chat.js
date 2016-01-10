6f9a1276f722099c614be7669541b86bf4055d4b
wissar <masterk850@gmail.com>
2016-01-10 16:52:34 -0600

    Chotango

--- curso_angular/chat.js
+++ curso_angular/chat.js
@@ -1,6 +1,14 @@
 angular.module('myApp', ['lumx', 'firebase'])
     .controller('ctrlChat', function($scope, $firebaseArray) {
-    	var ref = new Firebase('https://wissar-chat.firebaseio.com');
-    	$scope.messages = $firebaseArray(ref);
-    	console.log($scope.messages);
+        var ref = new Firebase('https://wissar-chat.firebaseio.com');
+        $scope.messages = $firebaseArray(ref);
+        console.log($scope.messages);
+        $scope.sendMessage = function() {
+            if ($scope.user.message) {
+                $scope.messages.$add({
+                    from: $scope.user.name,
+                    body: $scope.user.message
+                });
+            }
+        }
     })
